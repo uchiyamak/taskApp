14634cf08ecf9cfaf443e7c57783f55028299643
Kazuya Uchiyama <ucchi.k@gmail.com>
2018-08-04 01:13:21 +0900

    taskapp

--- taskapp/InputViewController.swift
+++ taskapp/InputViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import RealmSwift
+import UserNotifications
 
 class InputViewController: UIViewController {
 
@@ -43,7 +44,50 @@ class InputViewController: UIViewController {
             self.realm.add(self.task, update: true)
         }
         
+        setNotification(task: task)
+        
         super.viewWillDisappear(animated)
+    }
+    
+    //タスクのローカル通知を登録
+    func setNotification(task: Task) {
+        let content = UNMutableNotificationContent()
+        // タイトルと内容を設定（中身がない場合、メッセージなしで音だけの通知になるのでそれを回避
+        if task.title == "" {
+            content.title = "（タイトルなし）"
+        } else {
+            content.title = task.title
+        }
+        if task.contents == "" {
+            content.body = "（内容なし）"
+        } else {
+            content.body = task.contents
+        }
+        content.sound = UNNotificationSound.default()
+        
+        //ローカル通知が発動するtiggerを作成
+        let calendar = Calendar.current     //Calendarはどこで定義されてる？
+        let dateComponents = calendar.dateComponents([.year, .month, .day, .hour, .minute], from: task.date)    //どこをどうマッチさせてるのか？
+        let trigger = UNCalendarNotificationTrigger.init(dateMatching: dateComponents, repeats: false)
+        
+        //identifier, content, triggerからローカル通知を作成
+        let request = UNNotificationRequest.init(identifier: String(task.id), content: content, trigger: trigger)
+        
+        //ローカル通知を登録
+        let center = UNUserNotificationCenter.current()
+        center.add(request) { (error) in
+            print(error ?? "ローカル通知登録 OK")   //errorがnilなら成功を表示、エラーがあればerrorを表示
+        }       //どういう仕組み？
+        
+        //未通知のローカル通知一覧をログ出力
+        center.getPendingNotificationRequests { (requests: [UNNotificationRequest]) in
+            for request in requests {
+                print("/-------------/")
+                print(request)
+                print("/-------------/")
+            }
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
